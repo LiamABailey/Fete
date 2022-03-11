edddb78c887a6d1aca36aa6a0327768666a44605
LiamABailey <bailey.liam102@gmail.com>
2022-03-10 20:00:50 -0500

    Added FeatureDefinition struct. Initial planning for mongo db init script

--- api/fetestruct.go
+++ api/fetestruct.go
@@ -10,26 +10,42 @@ type Insertable interface {
   SetDateUpdated()
 }
 
-type FeatureDefinition struct {
-  // stores definition and metadata
+type DefinitionSubTest struct {
+  // each definition test has one or more subtests, which describe the
+  // specific test parameters
 }
 
-type Tag struct {
-  // defines tags for labeling features, environments
+func (dst *  DefinitionSubTest) SetDateAdded() {
+  // set the record instertion date
 }
 
-type FeatureEnvironment struct {
-  // stores environmental expectations for a feature
-  // defition (python version, packages with version)
+func (dst  *DefinitionSubTest) SetDateUpdated() {
+  // set the record update date
 }
-
 type DefinitionTest struct {
   // stores definition test information
 }
 
-type DefinitionSubTest struct {
-  // each definition test has one or more subtests, which describe the
-  // specific test parameters
+func (dt *  DefinitionTest) SetDateAdded() {
+  // set the record instertion date
+}
+
+func (dt *DefinitionTest) SetDateUpdated() {
+  // set the record update date
+}
+
+type FeatureDefinition struct {
+  ID                    bson.Objectid   `json:"id" bson:"_id"`
+  FeatureID             int             `json:"feature_id" bson:"id"`
+  VersionID             int             `json:"version_id" bson:"version_id"`
+  PackageId             bson.Objectid   `json:"package_id" bson:"package_id"`
+  FeatureEnvironmentID  bson.ObjectId   `json:"feature_environment_id" bson:"feature_environment_id"`
+  FeatureName           string          `json:"feature_name" bson:"feature_name"`
+  VersionName           string          `json:"version_name" bson:"version_name"`
+  InputArgs             []Variable      `json:"input_args" bson:"input_args"`
+  RetrunValues          []Variable      `json:"return_values" bson:"return_values"`
+  DateAdded             time.Time       `json:"date_added" bson:"date_added"`
+  DateUpdated           time.Time       `json:"date_updated" bson:"date_updated"`
 }
 
 type FeatureDefinitionRequestEventLog struct {
@@ -37,10 +53,50 @@ type FeatureDefinitionRequestEventLog struct {
   // and retrieval/test success
 }
 
-func (i *  Insertable) SetDateAdded() {
+func (l *  FeatureDefinitionRequestEventLog) SetDateAdded() {
   // set the record instertion date
 }
 
-func (I *Insertable) SetDateUpdated() {
+func (l *FeatureDefinitionRequestEventLog) SetDateUpdated() {
   // set the record update date
+}
+
+type FeatureEnvironment struct {
+  // stores environmental expectations for a feature
+  // defition (python version, packages with version)
+}
+
+func (fe *  FeatureEnvironment) SetDateAdded() {
+  // set the record instertion date
+}
+
+func (fe *FeatureEnvironment) SetDateUpdated() {
+  // set the record update date
+}
+
+func (fd *  FeatureDefinition) SetDateAdded() {
+  // set the record instertion date
+}
+
+func (fd *FeatureDefinition) SetDateUpdated() {
+  // set the record update date
+}
+
+type Tag struct {
+  // defines tags for labeling features, environments
+}
+
+func (t *  Tag) SetDateAdded() {
+  // set the record instertion date
+}
+
+func (t *Tag) SetDateUpdated() {
+  // set the record update date
+}
+
+type Variable struct {
+  // defines variables used to define name & type for function inputs
+  Name    string  `json:"name" bson:"name"`
+  Type    string  `json:"type" bson:"type"`
+  Default string  `json:"default" bson:"default"`
 }
---
+++ db/db_init.js
@@ -0,0 +1,5 @@
+// create connection to mongo db
+// check if database exists. If not create database
+// for each expected collection,
+//  check if exists. Create if not
+//  Check if index exists. Create if not
